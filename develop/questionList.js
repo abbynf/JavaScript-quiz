6d1ea4b83986508a7e05561b82afdcfc44a0f0dc
Abby Jo Frandsen <abbynfrandsen@gmail.com>
2020-08-14 20:32:50 -0600

    created js file to hold question list

---
+++ develop/questionList.js
@@ -0,0 +1,192 @@
+var questions = {
+  Q1: {
+    question: "JavaScript does which of the following?",
+    choice1: {
+      content: "1. It defines the content of the web pages",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. It specifies the layout of the web pages",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. It programs the behavior of the web pages",
+      accuracy: "true"
+    },
+    choice4: {
+      content: "4. It hosts the webpage",
+      accuracy: "false"
+    }
+  },
+  Q2: {
+    question: "JavaScript can select an element by using which method?",
+    choice1: {
+      content: "1. getElementById",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. getElementsByClassName",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. getElementsByTagName",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. All of the above",
+      accuracy: "true"
+    }
+  },
+  Q3: {
+    question: "Create an alert box by using this method",
+    choice1: {
+      content: "1. window.alert()",
+      accuracy: "true"
+    },
+    choice2: {
+      content: "2. console.log()",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. document.write()",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. innerHTML",
+      accuracy: "false"
+    }
+  },
+  Q4: {
+    question: "Which of the following NOT is a common data type?",
+    choice1: {
+      content: "1. holder",
+      accuracy: "true"
+    },
+    choice2: {
+      content: "2. variable",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. number",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. object",
+      accuracy: "false"
+    },
+  },
+  Q5: {
+    question: "5.	What is the keyword to declare a variable?",
+    choice1: {
+      content: "1. set",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. var",
+      accuracy: "true"
+    },
+    choice3: {
+      content: "3. know",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. as",
+      accuracy: "false"
+    },
+  },
+  Q6: {
+    question: "What are strings enclosed in?",
+    choice1: {
+      content: "1. dollar signs",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. quotation marks",
+      accuracy: "true"
+    },
+    choice3: {
+      content: "3. brackets",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. parentheses",
+      accuracy: "false"
+    },
+  },
+  Q7: {
+    question: "What keyword declares a function?",
+    choice1: {
+      content: "1. function",
+      accuracy: "true"
+    },
+    choice2: {
+      content: "2. var",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. let",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. when",
+      accuracy: "false"
+    },
+  },
+  Q8: {
+    question: "Objects can have:",
+    choice1: {
+      content: "1. properties",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. methods",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. values",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. all of the above",
+      accuracy: "true"
+    },
+  },
+  Q9: {
+    question: "Arrays are enclosed in:",
+    choice1: {
+      content: "1. quotation marks",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. asterisks",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. square brackets",
+      accuracy: "true"
+    },
+    choice4: {
+      content: "4. parentheses",
+      accuracy: "false"
+    },
+  },
+  Q10: {
+    question: "Which of the following is not an event?",
+    choice1: {
+      content: "1. onclick",
+      accuracy: "false"
+    },
+    choice2: {
+      content: "2. onmouseover",
+      accuracy: "false"
+    },
+    choice3: {
+      content: "3. onmouseout",
+      accuracy: "false"
+    },
+    choice4: {
+      content: "4. onexit",
+      accuracy: "true"
+    }
+  }
+}
--- develop/script.js
+++ develop/script.js
@@ -1,5 +1,30 @@
 var startButton = document.getElementById("start")
 
+
+function startTimer(){
+    console.log(window);
+    // added empty function to start the timer, i don't know how to do it yet
+}
+
+function listQuestions(){
+
+
+}
+
 startButton.addEventListener("click", function(){
+    // This function hides the welcome/starting screen
     document.getElementById("startingScreen").style.display = "none";
+
+    //Starts the display of the quiz questions
+    document.getElementById("quizQuestions").style.display = "block";
+
+    //Starts function that fills in the quiz questions div
+    listQuestions();
+
+    // Starts the timer in the upper right hand corner
+    startTimer();
+
+
 })
+
+console.log(questions.Q1.choice1.accuracy)
